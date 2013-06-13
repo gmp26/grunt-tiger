6102bf0fa96485000b2f5391fc9cd761422dd461
gmp26 <gmp26@cam.ac.uk>
2013-06-13 15:04:21 +0100

    first commit

---
+++ test/tiger_test.js
@@ -0,0 +1,26 @@
+(function(){
+  "use strict";
+  var grunt;
+  grunt = require("grunt");
+  exports.tiger = {
+    setUp: function(done){
+      return done();
+    },
+    default_options: function(test){
+      var actual, expected;
+      test.expect(1);
+      actual = grunt.file.read("tmp/default_options");
+      expected = grunt.file.read("test/expected/default_options");
+      test.equal(actual, expected, "should describe what the default behavior is.");
+      return test.done();
+    },
+    custom_options: function(test){
+      var actual, expected;
+      test.expect(1);
+      actual = grunt.file.read("tmp/custom_options");
+      expected = grunt.file.read("test/expected/custom_options");
+      test.equal(actual, expected, "should describe what the custom option(s) behavior is.");
+      return test.done();
+    }
+  };
+}).call(this);
